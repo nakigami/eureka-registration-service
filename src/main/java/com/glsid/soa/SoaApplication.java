740a21f5bda471c57ce5ae359c514ff75558130a
Anas RIANI <anas.devriani@gmail.com>
2020-12-12 15:02:19 +0100

    Creation of eureka discovery service

---
+++ src/main/java/com/glsid/soa/SoaApplication.java
@@ -0,0 +1,15 @@
+package com.glsid.soa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaApplication.class, args);
+    }
+
+}
